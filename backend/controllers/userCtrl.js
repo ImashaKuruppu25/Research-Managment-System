9f69a3d24530d5aa31c914866dcc5338877ee4e4
Chanuth10 <it20074418@my.sliit.lk>
2022-04-18 03:09:33 +0530

    Generate JsonWebToken link

--- backend/controllers/userCtrl.js
+++ backend/controllers/userCtrl.js
@@ -1,35 +1,50 @@
 const Users = require('../models/userModel')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const userCtrl = {
-     register: async (req, res) =>{
-         try{
-             const {name, email, password} = req.body;
+        register: async (req, res) =>{
+            try{
+                const {name, email, password} = req.body;
+   
+                const user = await Users.findOne({email})
+                if(user) return res.status(400).json({msg: "The email already exists."})
+   
+                if(password.length < 6)
+                    return res.status(400).json({msg:"Password not strong."})
+   
+                // password Encryption.    
+                const passwordHash = await bcrypt.hash(password, 10) 
+                const newUser = new Users({
+                    name, email, password: passwordHash
+                })
+   
+   
+                await newUser.save()
+                
+                // Then create isonwebtoken to authentication
+                const accesstoken = createAccessToken({id: newUser._id})
+                const refreshtoken = createRefreshToken({id: newUser._id})
+   
+                res.cookie('refreshtoken', refreshtoken, {
+                    httpOnly: true,
+                    path: '/user/refresh_token'
+                })
+                res.json({accesstoken})
+                //res.json({msg: "Register Success!"})
+                
+            }catch(err){
+                return res.status(500).json({msg: err.message})
+            }
+        }
+}
 
-             const user = await Users.findOne({email})
-             if(user) return res.status(400).json({msg: "The email already exists."})
+const createAccessToken = (user) =>{
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'})
+}
 
-             if(password.length < 6)
-                 return res.status(400).json({msg:"Password not strong."})
-
-             res.json({msg: "Register Success."})     
-
-             // password Encryption.    
-             const passwordHash = await bcrypt.hash(password, 10) 
-             //res.json({password, passwordHash})    
-             //   res.json({msg: "Register Success."})  
-
-             const newUser = new Users({
-                name, email, password: passwordHash
-            })
-            //res.json(newUser)
-
-            await newUser.save()
-
-         }catch(err){
-             return res.status(500).json({msg: err.message})
-         }
-     }
+const createRefreshToken = (user) =>{
+    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'})
 }
 
 module.exports = userCtrl
