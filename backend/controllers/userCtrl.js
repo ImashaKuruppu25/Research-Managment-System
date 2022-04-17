4632660c39758ccb5b0a2761250f8033572468a9
Chanuth10 <it20074418@my.sliit.lk>
2022-04-18 02:46:40 +0530

    Save user details into the mongodb

--- backend/controllers/userCtrl.js
+++ backend/controllers/userCtrl.js
@@ -1,7 +1,22 @@
+const Users = require('../models/userModel')
+
 const userCtrl = {
-    register: (req, res) =>{
-        res.json({msg: "Test Controllers"})
-    }
+     register: async (req, res) =>{
+         try{
+             const {name, email, password} = req.body;
+
+             const user = await Users.findOne({email})
+             if(user) return res.status(400).json({msg: "The email already exists."})
+
+             if(password.length < 6)
+                 return res.status(400).json({msg:"Password not strong."})
+
+             res.json({msg: "Register Success."})     
+
+         }catch(err){
+             return res.status(500).json({msg: err.message})
+         }
+     }
 }
 
-module.exports = userCtrl 
+module.exports = userCtrl
