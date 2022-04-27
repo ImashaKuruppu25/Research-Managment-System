32026b62c93d70cdcee49c1517f71fb702740660
Hasith Deminda <it20238612@my.sliit.lk>
2022-04-27 17:00:05 +0530

    Admin Dashboard UI Implemented

---
+++ Admin-Dashboard/src/components/revenue-list/RevenueList.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { data } from "../../constants";
+import "./revenue-list.scss";
+import ProgressBar from "../progressbar/ProgressBar";
+
+const RevenueList = () => {
+  return (
+    <div className="paper-submissions-container">
+      <ul
+        className="revenue-list"
+        style={{ overflowY: "auto", maxHeight: "200px" }}
+      >
+        {data.revenueByChannel.map((item, index) => (
+          <li
+            className="revenue-list__item"
+            key={`revenue-${index}`}
+            style={{ margin: "20px 15px" }}
+          >
+            <div className="revenue-list__item__title">
+              {item.title}
+              <span
+                className={`${item.value > 50 ? "txt-success" : "txt-danger"}`}
+              >
+                {item.value}%
+              </span>
+            </div>
+            <div>
+              <ProgressBar value={item.value} />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RevenueList;
---
+++ Admin-Dashboard/src/constants/images.js
@@ -0,0 +1,6 @@
+const images = {
+  logo: require("../assets/images/logo.svg").default,
+  avt: require("https://res.cloudinary.com/desnqqj6a/image/upload/v1649172476/My_Img_qvpnz3.jpg"),
+};
+
+export default images;
