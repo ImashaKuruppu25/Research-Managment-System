42d7d5b6418eac95b77ee60fa7c58baffcce941b
Hasith Deminda <it20238612@my.sliit.lk>
2022-05-01 21:16:21 +0530

    Backend functionality of User details display, update, delete implemented!

--- Admin-Dashboard/src/pages/Users/Users.jsx
+++ Admin-Dashboard/src/pages/Users/Users.jsx
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import "./Users.scss";
 import { MdDelete } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Users = () => {
+  const [users, setUsers] = useState([
+    {
+      name: "",
+      email: "",
+      phone: "",
+      role: "",
+      image: "",
+    },
+  ]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getUsers = async () => {
+      await axios
+        .get("http://localhost:5000/admin/adminGetAllUsers")
+
+        .then((res) => {
+          setUsers(res.data);
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    };
+    getUsers();
+  }, []);
+
+  //Delete a User
+  function deleteHandler(_id) {
+    axios
+      .delete(`http://localhost:5000/admin/adminDeleteUser/${_id}`)
+
+      .then((res) => {
+        alert("User Deleted!");
+
+        navigate("/users");
+      })
+
+      .catch();
+  }
+
   const columns = [
     { field: "id", headerName: "ID", width: 70 },
     {
@@ -33,7 +74,10 @@ const Users = () => {
               <button className="userlist-edit-btn">Update</button>
             </Link>
 
-            <MdDelete className="userlist-delete-btn" />
+            <MdDelete
+              className="userlist-delete-btn"
+              onClick={() => deleteHandler(users._id)}
+            />
           </div>
         );
       },
@@ -43,142 +87,6 @@ const Users = () => {
   const rows = [
     {
       id: 1,
-      avatar:
-        "https://res.cloudinary.com/desnqqj6a/image/upload/v1650175831/fitness_dmymhn.png",
-      userName: "Hasith Deminda",
-      email: "Hasith@gmail.com",
-      role: "STUDENT",
-    },
-    {
-      id: 2,
-      avatar:
-        "https://res.cloudinary.com/desnqqj6a/image/upload/v1650175831/fitness_dmymhn.png",
-      userName: "Hasith Deminda",
-      email: "Hasith@gmail.com",
-      role: "STUDENT",
-    },
-    {
-      id: 3,
-      avatar:
-        "https://res.cloudinary.com/desnqqj6a/image/upload/v1650175831/fitness_dmymhn.png",
-      userName: "Hasith Deminda",
-      email: "Hasith@gmail.com",
-      role: "STUDENT",
-    },
-    {
-      id: 4,
-      avatar:
-        "https://res.cloudinary.com/desnqqj6a/image/upload/v1650175831/fitness_dmymhn.png",
-      userName: "Hasith Deminda",
-      email: "Hasith@gmail.com",
-      role: "STUDENT",
-    },
-    {
-      id: 5,
-      avatar:
-        "https://res.cloudinary.com/desnqqj6a/image/upload/v1650175831/fitness_dmymhn.png",
-      userName: "Hasith Deminda",
-      email: "Hasith@gmail.com",
-      role: "STUDENT",
-    },
-    {
-      id: 6,
-      avatar:
-        "https://res.cloudinary.com/desnqqj6a/image/upload/v1650175831/fitness_dmymhn.png",
-      userName: "Hasith Deminda",
-      email: "Hasith@gmail.com",
-      role: "STUDENT",
-    },
-    {
-      id: 7,
-      avatar:
-        "https://res.cloudinary.com/desnqqj6a/image/upload/v1650175831/fitness_dmymhn.png",
-      userName: "Hasith Deminda",
-      email: "Hasith@gmail.com",
-      role: "STUDENT",
-    },
-    {
-      id: 8,
-      avatar:
-        "https://res.cloudinary.com/desnqqj6a/image/upload/v1650175831/fitness_dmymhn.png",
-      userName: "Hasith Deminda",
-      email: "Hasith@gmail.com",
-      role: "STUDENT",
-    },
-    {
-      id: 9,
-      avatar:
-        "https://res.cloudinary.com/desnqqj6a/image/upload/v1650175831/fitness_dmymhn.png",
-      userName: "Hasith Deminda",
-      email: "Hasith@gmail.com",
-      role: "STUDENT",
-    },
-    {
-      id: 10,
-      avatar:
-        "https://res.cloudinary.com/desnqqj6a/image/upload/v1650175831/fitness_dmymhn.png",
-      userName: "Hasith Deminda",
-      email: "Hasith@gmail.com",
-      role: "STUDENT",
-    },
-    {
-      id: 11,
-      avatar:
-        "https://res.cloudinary.com/desnqqj6a/image/upload/v1650175831/fitness_dmymhn.png",
-      userName: "Hasith Deminda",
-      email: "Hasith@gmail.com",
-      role: "STUDENT",
-    },
-    {
-      id: 12,
-      avatar:
-        "https://res.cloudinary.com/desnqqj6a/image/upload/v1650175831/fitness_dmymhn.png",
-      userName: "Hasith Deminda",
-      email: "Hasith@gmail.com",
-      role: "STUDENT",
-    },
-    {
-      id: 13,
-      avatar:
-        "https://res.cloudinary.com/desnqqj6a/image/upload/v1650175831/fitness_dmymhn.png",
-      userName: "Hasith Deminda",
-      email: "Hasith@gmail.com",
-      role: "STUDENT",
-    },
-    {
-      id: 14,
-      avatar:
-        "https://res.cloudinary.com/desnqqj6a/image/upload/v1650175831/fitness_dmymhn.png",
-      userName: "Hasith Deminda",
-      email: "Hasith@gmail.com",
-      role: "STUDENT",
-    },
-    {
-      id: 15,
-      avatar:
-        "https://res.cloudinary.com/desnqqj6a/image/upload/v1650175831/fitness_dmymhn.png",
-      userName: "Hasith Deminda",
-      email: "Hasith@gmail.com",
-      role: "STUDENT",
-    },
-    {
-      id: 16,
-      avatar:
-        "https://res.cloudinary.com/desnqqj6a/image/upload/v1650175831/fitness_dmymhn.png",
-      userName: "Hasith Deminda",
-      email: "Hasith@gmail.com",
-      role: "STUDENT",
-    },
-    {
-      id: 17,
-      avatar:
-        "https://res.cloudinary.com/desnqqj6a/image/upload/v1650175831/fitness_dmymhn.png",
-      userName: "Hasith Deminda",
-      email: "Hasith@gmail.com",
-      role: "STUDENT",
-    },
-    {
-      id: 18,
       avatar:
         "https://res.cloudinary.com/desnqqj6a/image/upload/v1650175831/fitness_dmymhn.png",
       userName: "Hasith Deminda",
--- backend/server.js
+++ backend/server.js
@@ -10,22 +10,25 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to mongodb
-const URI = process.env.MONGO_URL
-mongoose.connect(URI, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-}, err =>{
-    if(err) throw err;
-    console.log('Connected to MongoDb')
-}) 
-
-
-
+const URI = process.env.MONGO_URL;
+mongoose.connect(
+  URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Database Connection Successfull!");
+  }
+);
 
 // Router
-app.use('/user', require('./routes/userRoute'))
+app.use("/user", require("./routes/userRoute"));
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () =>{
-    console.log('Server is running on port', PORT)
-})
+app.use("/admin", require("./routes/adminUserRoute"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server is running on PORT :", PORT);
+});
